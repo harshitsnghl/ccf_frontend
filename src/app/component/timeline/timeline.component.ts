25639bfcfb6f6488dbf5798e2d84adb6379a4df6
Harshit Singhal <harshitsnghl@gmail.com>
2022-08-30 03:00:20 +0530

    Tags fixed

--- src/app/component/timeline/timeline.component.ts
+++ src/app/component/timeline/timeline.component.ts
@@ -1,5 +1,7 @@
+import { UserResponse } from './../../model/user-response';
+import { UserService } from './../../service/user.service';
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { noop, Subscription } from 'rxjs';
@@ -10,136 +12,160 @@ import { AuthService } from 'src/app/service/auth.service';
 import { PostService } from 'src/app/service/post.service';
 import { TimelineService } from 'src/app/service/timeline.service';
 import { SnackbarComponent } from '../snackbar/snackbar.component';
+import { User } from 'src/app/model/user';
 
 @Component({
-	selector: 'app-timeline',
-	templateUrl: './timeline.component.html',
-	styleUrls: ['./timeline.component.css']
+  selector: 'app-timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.css'],
 })
 export class TimelineComponent implements OnInit, OnDestroy {
-	timelinePostResponseList: PostResponse[] = [];
-	timelineTagList: Tag[] = [];
-	noPost: boolean = false;
-	resultPage: number = 1;
-	resultSize: number = 5;
-	hasMoreResult: boolean = true;
-	fetchingResult: boolean = false;
-	isTaggedPostPage: boolean = false;
-	targetTagName: string;
-	loadingTimelinePostsInitially: boolean = true;
-	loadingTimelineTagsInitially: boolean = true;
+  timelinePostResponseList: PostResponse[] = [];
+  timelineTagList: Tag[] = [];
+  noPost: boolean = false;
+  resultPage: number = 1;
+  resultSize: number = 5;
+  hasMoreResult: boolean = true;
+  hasMoreFollowers: boolean = true;
+  fetchingResult: boolean = false;
+  isTaggedPostPage: boolean = false;
+  targetTagName: string;
+  loadingTimelinePostsInitially: boolean = true;
+  loadingTimelineTagsInitially: boolean = true;
+  allPost: boolean = true;
+  profileUser: User;
+  userResponseList: UserResponse[] = [];
 
-	private subscriptions: Subscription[] = [];
+  private subscriptions: Subscription[] = [];
 
-	constructor(
-		private authService: AuthService,
-		private timelineService: TimelineService,
-		private postService: PostService,
-		private router: Router,
-		private activatedRoute: ActivatedRoute,
-		private matSnackbar: MatSnackBar) { }
+  constructor(
+    private authService: AuthService,
+    private timelineService: TimelineService,
+    private postService: PostService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private matSnackbar: MatSnackBar
+  ) {}
 
-	ngOnInit(): void {
-		if (!this.authService.isUserLoggedIn()) {
-			this.router.navigateByUrl('/login');
-		} else {
-			if (this.router.url !== '/') {
-				this.targetTagName = this.activatedRoute.snapshot.paramMap.get('tagName');
-				this.isTaggedPostPage = true;
-				this.loadTaggedPosts(this.targetTagName, 1);
-			} else {
-				this.loadTimelinePosts(1);
-			}
+  ngOnInit(): void {
+    if (!this.authService.isUserLoggedIn()) {
+      this.router.navigateByUrl('/login');
+    } else {
+      if (this.router.url !== '/') {
+        this.targetTagName =
+          this.activatedRoute.snapshot.paramMap.get('tagName');
+        this.isTaggedPostPage = true;
+        this.loadTaggedPosts(this.targetTagName, 1);
+      } else {
+        this.loadTimelinePosts(1);
+      }
 
-			this.loadTimelineTags();
-		}
-	}
+      this.loadTimelineTags();
+    }
+  }
 
-	ngOnDestroy(): void {
-		this.subscriptions.forEach(sub => sub.unsubscribe());
-	}
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
 
-	loadTimelinePosts(currentPage: number): void {
-		if (!this.fetchingResult) {
-			this.fetchingResult = true;
-			this.subscriptions.push(
-				this.timelineService.getTimelinePosts(currentPage, this.resultSize).subscribe({
-					next: (postResponseList: PostResponse[]) => {
-						if (postResponseList.length === 0 && currentPage === 1) this.noPost = true;
-						
-						postResponseList.forEach(pR => this.timelinePostResponseList.push(pR));
+  loadTimelinePosts(currentPage: number): void {
+    if (!this.fetchingResult) {
+      this.fetchingResult = true;
+      this.subscriptions.push(
+        this.timelineService
+          .getTimelinePosts(currentPage, this.resultSize, this.allPost)
+          .subscribe({
+            next: (postResponseList: PostResponse[]) => {
+              if (postResponseList.length === 0 && currentPage === 1)
+                this.noPost = true;
+              postResponseList.forEach((pR) =>
+                this.timelinePostResponseList.push(pR)
+              );
+              if (postResponseList.length > 0) {
+                this.hasMoreResult = true;
+              } else {
+                this.hasMoreResult = false;
+              }
+              this.resultPage++;
+              this.fetchingResult = false;
+              this.loadingTimelinePostsInitially = false;
+            },
+            error: (errorResponse: HttpErrorResponse) => {
+              this.matSnackbar.openFromComponent(SnackbarComponent, {
+                data: AppConstants.snackbarErrorContent,
+                panelClass: ['bg-danger'],
+                duration: 5000,
+              });
+              this.fetchingResult = false;
+            },
+          })
+      );
+    }
+  }
 
-						if (postResponseList.length > 0) {
-							this.hasMoreResult = true;
-						} else {
-							this.hasMoreResult = false;
-						}
+  loadTaggedPosts(tagName: string, currentPage: number): void {
+    if (!this.fetchingResult) {
+      this.fetchingResult = true;
+      this.subscriptions.push(
+        this.postService
+          .getPostsByTag(tagName, currentPage, this.resultSize, this.allPost)
+          .subscribe({
+            next: (postResponseList: PostResponse[]) => {
+              if (postResponseList.length === 0 && currentPage === 1)
+                this.noPost = true;
 
-						this.resultPage++;
-						this.fetchingResult = false;
-						this.loadingTimelinePostsInitially = false;
-					},
-					error: (errorResponse: HttpErrorResponse) => {
-						this.matSnackbar.openFromComponent(SnackbarComponent, {
-							data: AppConstants.snackbarErrorContent,
-							panelClass: ['bg-danger'],
-							duration: 5000
-						});
-						this.fetchingResult = false;
-					}
-				})
-			);
-		}
-	}
+              postResponseList.forEach((pR) =>
+                this.timelinePostResponseList.push(pR)
+              );
+              if (postResponseList.length > 0) {
+                this.hasMoreResult = true;
+              } else {
+                this.hasMoreResult = false;
+              }
+              this.resultPage++;
+              this.fetchingResult = false;
+              this.loadingTimelinePostsInitially = false;
+            },
+            error: (errorResponse: HttpErrorResponse) => {
+              this.matSnackbar.openFromComponent(SnackbarComponent, {
+                data: AppConstants.snackbarErrorContent,
+                panelClass: ['bg-danger'],
+                duration: 5000,
+              });
+              this.fetchingResult = false;
+            },
+          })
+      );
+    }
+  }
 
-	loadTaggedPosts(tagName: string, currentPage: number): void {
-		if (!this.fetchingResult) {
-			this.fetchingResult = true;
-			this.subscriptions.push(
-				this.postService.getPostsByTag(tagName, currentPage, this.resultSize).subscribe({
-					next: (postResponseList: PostResponse[]) => {
-						if (postResponseList.length === 0 && currentPage === 1) this.noPost = true;
-						
-						postResponseList.forEach(pR => this.timelinePostResponseList.push(pR));
-						if (postResponseList.length > 0) {
-							this.hasMoreResult = true;
-						} else {
-							this.hasMoreResult = false;
-						}
-						this.resultPage++;
-						this.fetchingResult = false;
-					},
-					error: (errorResponse: HttpErrorResponse) => {
-						this.matSnackbar.openFromComponent(SnackbarComponent, {
-							data: AppConstants.snackbarErrorContent,
-							panelClass: ['bg-danger'],
-							duration: 5000
-						});
-						this.fetchingResult = false;
-					}
-				})
-			);
-		}
-	}
+  loadTimelineTags(): void {
+    this.fetchingResult = true;
+    this.subscriptions.push(
+      this.timelineService.getTimelineTags().subscribe({
+        next: (tagList: Tag[]) => {
+          tagList.forEach((t) => this.timelineTagList.push(t));
+          this.loadingTimelineTagsInitially = false;
+          this.fetchingResult = false;
+        },
+        error: (errorResponse: HttpErrorResponse) => {
+          this.matSnackbar.openFromComponent(SnackbarComponent, {
+            data: AppConstants.snackbarErrorContent,
+            panelClass: ['bg-danger'],
+            duration: 5000,
+          });
+          this.fetchingResult = false;
+        },
+      })
+    );
+  }
 
-	loadTimelineTags(): void {
-		this.fetchingResult = true;
-		this.subscriptions.push(
-			this.timelineService.getTimelineTags().subscribe({
-				next: (tagList: Tag[]) => {
-					tagList.forEach(t => this.timelineTagList.push(t));
-					this.loadingTimelineTagsInitially = false;
-					this.fetchingResult = false;
-				},
-				error: (errorResponse: HttpErrorResponse) => {
-					this.matSnackbar.openFromComponent(SnackbarComponent, {
-						data: AppConstants.snackbarErrorContent,
-						panelClass: ['bg-danger'],
-						duration: 5000
-					});
-					this.fetchingResult = false;
-				}
-			})
-		);
-	}
+  togglePost(bool: boolean) {
+    this.allPost = bool;
+    this.userResponseList = [];
+    this.timelinePostResponseList = [];
+    this.timelineTagList = [];
+    this.resultPage = 1;
+    this.ngOnInit();
+  }
 }
--- src/app/service/timeline.service.ts
+++ src/app/service/timeline.service.ts
@@ -1,4 +1,8 @@
-import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -6,19 +10,29 @@ import { PostResponse } from '../model/post-response';
 import { Tag } from '../model/tag';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root',
 })
 export class TimelineService {
-	private host = environment.apiUrl;
+  private host = environment.apiUrl;
 
-	constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-	getTimelinePosts(page: number, size: number): Observable<PostResponse[] | HttpErrorResponse> {
-		const reqParams = new HttpParams().set('page', page).set('size', size);
-		return this.httpClient.get<PostResponse[] | HttpErrorResponse>(`${this.host}/`, { params: reqParams });
-	}
+  getTimelinePosts(
+    page: number,
+    size: number,
+    allPost: boolean
+  ): Observable<PostResponse[] | HttpErrorResponse> {
+    const reqParams = new HttpParams()
+      .set('page', page)
+      .set('size', size)
+      .set('allPost', allPost);
+    return this.httpClient.get<PostResponse[] | HttpErrorResponse>(
+      `${this.host}/`,
+      { params: reqParams }
+    );
+  }
 
-	getTimelineTags(): Observable<Tag[] | HttpErrorResponse> {
-		return this.httpClient.get<Tag[] | HttpErrorResponse>(`${this.host}/tags`);
-	}
+  getTimelineTags(): Observable<Tag[] | HttpErrorResponse> {
+    return this.httpClient.get<Tag[] | HttpErrorResponse>(`${this.host}/tags`);
+  }
 }
