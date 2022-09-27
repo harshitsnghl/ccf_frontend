917e8a92104e6d1122970024a8b3f4b384741047
Harshit Singhal <harshitsnghl@gmail.com>
2022-09-27 21:18:25 +0530

    Git Init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:8080/api/v1',
+  defaultProfilePhotoUrl: 'http://localhost:4200/assets/images/avatar.jpg',
+  defaultCoverPhotoUrl: 'http://localhost:4200/assets/images/cover.jpg',
 };
